79fb5ee1060b5bb10cd8b7fce0b27392b9384090
05487 <05487@farnborough.ac.uk>
2020-11-13 13:41:13 +0000

    New Project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,26 @@
-import java.util.Spliterator;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        Scanner Scanned = new Scanner(System.in);
+        Integer Num1 = 0;
+        Integer Num2 = 0;
+        Integer Num3 = 0;
+        Integer Num4 = 0;
 
-        String greeting = "Hello World";
+        System.out.println("Please enter a 4 digit binary number");
+        String Str = Scanned.next();
 
-        System.out.println(greeting);
-        System.out.println(greeting.length());
+        Num1 = Integer.valueOf(Str.substring(0, 1));
+        Num2 = Integer.valueOf(Str.substring(1, 2));
+        Num3 = Integer.valueOf(Str.substring(2, 3));
+        Num4 = Integer.valueOf(Str.substring(3, 4));
 
+        Num2 = Num2 * 2;
+        Num3 = Num3 * 4;
+        Num4 = Num4 * 8;
+
+        System.out.println("Your number converted to Denary is: " + Num1 + Num2 + Num3 + Num4);
     }
 }
